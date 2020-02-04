f1613d290b9b6852bdde861a60b0f7db6c1a526a
Jeremy Valenzuela <jevalen4868@yahoo.com>
2020-02-03 21:43:42 -0600

    BT20 : Created AI tank player classes.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay() {
+    Super::BeginPlay();
+    UE_LOG(LogTemp, Warning, TEXT("AI controller beginPlay"));
+    auto ControlledTank {GetControlledTank()};
+    if(!ControlledTank) {
+        UE_LOG(LogTemp, Warning, TEXT("AI controller not possessing a tank"));      
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("AI controller possessing: %s"), *(ControlledTank->GetName()));
+    }
+
+    auto PlayerTank {GetPlayerTank()};
+
+    if(!PlayerTank) {
+        UE_LOG(LogTemp, Warning, TEXT("AI controller not targeting the player tank"));      
+    } else {
+        UE_LOG(LogTemp, Warning, TEXT("AI controller targeting: %s"), *(PlayerTank->GetName()));
+    }
+    
+    //UE_LOG(LogTemp, Warning, TEXT());    
+}
+
+ATank* ATankAIController::GetControlledTank() const {
+    return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+    auto PlayerPawn {GetWorld()->GetFirstPlayerController()->GetPawn()};
+    return Cast<ATank>(PlayerPawn);
+}
